1a6e120e31aa889ea26cf0bbf636b01af428ab70
zanra2401 <grubgrex@gmail.com>
2025-06-22 11:48:31 +0700

    membuat ui lebih user friendly

--- app.py
+++ app.py
@@ -148,9 +148,12 @@ if predict_button and model is not None:
     
     # Asumsi: 0 = Tidak Sakit Jantung, 1 = Sakit Jantung
     if prediction[0] == 1:
-        st.error("**Risiko Tinggi: Terdeteksi Penyakit Jantung**", icon="�")
+        st.error("**Risiko Tinggi: Terdeteksi Penyakit Jantung**", icon="☠️")
+        st.error("**Pasien Terklasifikasi Ke Kelas ", prediction[0])
     else:
         st.success("**Risiko Rendah: Tidak Terdeteksi Penyakit Jantung**", icon="❤️")
+        st.error("**Pasien Terklasifikasi Ke Kelas ", prediction[0])
+
         
     # Tampilkan probabilitas
     st.write("Probabilitas Prediksi:")
